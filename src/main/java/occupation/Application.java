25772fe7e383eb6b849ee29b4b5d64db19225653
choshika <choshika@CHOSHIKA-BR.br.oracle.com>
2018-01-05 14:46:07 -0200

    Teste

--- src/main/java/occupation/Application.java
+++ src/main/java/occupation/Application.java
@@ -2,11 +2,27 @@ package occupation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 public class Application {
 
+    //public static String protocol = "";
+    //public static Optional<String> host = null;
+    public static Optional<String> port = null;
+    
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	SpringApplication application = new SpringApplication(Application.class);
+    
+        //protocol = "http://";
+        //host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+        
+    	Map<String, Object> map = new HashMap<>();
+		map.put("SERVER_PORT", port);
+		application.setDefaultProperties(map);
+        application.run(args);
     }
 }
