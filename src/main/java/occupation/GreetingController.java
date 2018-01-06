5e9d014ee1a702879eb5ebe3af0c37b44912e326
choshika <choshika@CHOSHIKA-BR.br.oracle.com>
2018-01-06 14:38:06 -0200

    Teste

--- src/main/java/occupation/GreetingController.java
+++ src/main/java/occupation/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Ola, %s!";
+    private static final String template = "Oi, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
