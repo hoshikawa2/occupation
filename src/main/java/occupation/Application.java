af3032c3c7c575a818ef6ca3076bab75654da504
choshika <choshika@CHOSHIKA-BR.br.oracle.com>
2018-01-06 08:33:59 -0200

    Teste

--- src/main/java/occupation/Application.java
+++ src/main/java/occupation/Application.java
@@ -9,15 +9,11 @@ import java.util.Optional;
 @SpringBootApplication
 public class Application {
 
-    //public static String protocol = "";
-    //public static Optional<String> host = null;
     public static Optional<String> port = null;
     
     public static void main(String[] args) {
     	SpringApplication application = new SpringApplication(Application.class);
     
-        //protocol = "http://";
-        //host = Optional.ofNullable(System.getenv("HOSTNAME"));
         port = Optional.ofNullable(System.getenv("PORT"));
         
     	Map<String, Object> map = new HashMap<>();
